3e0e954a8ace40d9f5309be7b3a8cc6e47e82bfd
KUNAL-KUMAR-SINGH-Coder <mrg7216@gmail.com>
2024-11-09 19:14:37 +0530

    Add files via upload

---
+++ Program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main(){
+    char C1='a';
+    unsigned char C2='b';
+    short int A=2;
+    printf("Size of Char:%d",sizeof(C1));
+    printf("\nSize of Unsigned Char: %d",sizeof(C2));
+    printf("\nSize of Short Int:%d",sizeof(A));
+
+
+}
